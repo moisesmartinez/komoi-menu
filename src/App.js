ef2b78dbc95a4b8df4f042444f4a181fb9175288
Moises Martínez <moy_sistemas@hotmail.com>
2023-05-11 00:19:34 -0600

    Adding menu.json and displaying the daily menu

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import Menu from './menu.json';
 
 function App() {
+  const [selectedDay, setSelectedDay] = useState('Lunes');
+
+  useEffect(() => {
+    const daysInSpanish = {
+      Sunday: "Domingo",
+      Monday: "Lunes",
+      Tuesday: "Martes",
+      Wednesday: "Miércoles",
+      Thursday: "Jueves",
+      Friday: "Viernes",
+      Saturday: "Sábado"
+    };
+    const today = new Date().toLocaleDateString('en-US', { weekday: 'long' });
+    setSelectedDay(daysInSpanish[today]);
+  }, []);
+
+  const handleSelectDay = (event) => {
+    setSelectedDay(event.target.value);
+  }
+
+  const mealKeys = Object.keys(Menu[selectedDay]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Mi Amasssss!! 
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <select value={selectedDay} onChange={handleSelectDay}>
+        {Object.keys(Menu).map((day) => (
+          <option key={day} value={day}>
+            {day}
+          </option>
+        ))}
+      </select>
+      <div className="meals-container">
+        {mealKeys.map((mealKey) => (
+          <div className="meal" key={mealKey}>
+            <h2>{mealKey}</h2>
+            {Menu[selectedDay][mealKey].split('\n').map((mealText, index) => (
+  <p key={index}>{mealText}</p>
+))}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
